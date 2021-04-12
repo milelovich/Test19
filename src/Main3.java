0799665685efc92309e1f63c445b52b2d7791469
milelovich <milelovich@mail.ru>
2021-04-12 18:35:14 +0300

    finished

--- src/Main3.java
+++ src/Main3.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main3 {
 
+
     public static void main(String[] args) {
 
         List<String> colors = Arrays.asList("Red", "Blue", "Green", "Green", "Black", "Red",
